15db99fadac26bec26329b5c31c3edba97ded231
syahrinseth <syahrinsethservices@gmail.com>
2026-02-26 12:28:35 +0800

    fix: Ensure IdentifyTenant middleware is applied to web routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->api(prepend: [
             IdentifyTenant::class,
+        ])->web(prepend: [
+            IdentifyTenant::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
